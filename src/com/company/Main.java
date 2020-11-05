3e5d41403608ea4b7a61b90f4dc28bb4789daf94
Lukas Ferda <LFerda@moravia.com>
2020-11-05 19:27:29 +0100

    add ode from lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //nastavte si prosím cestu k svemu driveru
         System.setProperty("webdriver.gecko.driver", "C:\\DevGit\\geckodriver.exe");
 
@@ -21,11 +21,20 @@ public class Main {
             browser.navigate().to("http://czechitas-shopizer.azurewebsites.net/shop/");
 
             //search box element
-            WebElement element = browser.findElement(By.id("searchField"));
+            WebElement searchBox = browser.findElement(By.className("typeahead tt-input"));
+
+            // vrati hodnotu atributu id a ulozi ji do promene idValue
+            String idValue = searchBox.getAttribute("id");
+
+            // zapise do searchboxu string "ahoj"
+            searchBox.sendKeys("ahoj");
+
+            Thread.sleep(5000);
 
         } catch (Exception ex) {
             // catch blok slouzi ke zpracovani vyjimky, v nasem pripade nechame vyjimku byt
             System.out.println("Bohuzel doslo k chybe. Jedna z poslednich informaci by mel byt: (Main.java:**cislo radku kde chyba vznikla**) ");
+            throw ex;
 
         } finally {
             // blok finally nam v tomto pripade poslouzi k tomu aby se WebDriver ukoncil spravne za kazdych okolnosti
